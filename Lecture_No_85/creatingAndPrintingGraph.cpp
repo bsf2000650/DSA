7ce182742faa7cb77f1256d75f862f1dbe2fa37c
asad <bsf2000650@ue.edu.pk>
2024-04-23 00:06:29 -0700

    Done with Lecture No 85 and 86

---
+++ Lecture_No_85/creatingAndPrintingGraph.cpp
@@ -0,0 +1,28 @@
+
+
+vector <vector <int > > printAdjacency(int n, int m, vector < vector < int > > &edges){
+    vector <int> ans;
+    // ans array will all adjacent nodes
+    for (int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        ans[u].push_back(v);
+        ans[v].push_back(u);
+    }
+
+    vector<vector < int > > ajd(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        adj[i].push_back(i);
+
+        //entering neighbours
+        for (int j = 0; i < ans[i].size(); i++)
+        {
+            adj[i].push_back(ans[i][j]);
+        }
+    }
+    return adj;  
+}
